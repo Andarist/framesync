8f80dd8eeabefac2edcfc99f85476cdf172a6101
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-05-18 15:28:29 +0200

    Annotate #__PURE__ calls automatically with a babel plugin

--- dist/framesync.es.js
+++ dist/framesync.es.js
@@ -1,13 +1,15 @@
 var hasRAF = typeof window !== 'undefined' && window.requestAnimationFrame !== undefined;
 var prevTime = 0;
-var onNextFrame = hasRAF
-    ? function (callback) { return window.requestAnimationFrame(callback); }
-    : function (callback) {
-        var currentTime = Date.now();
-        var timeToCall = Math.max(0, 16.7 - (currentTime - prevTime));
-        prevTime = currentTime + timeToCall;
-        setTimeout(function () { return callback(prevTime); }, timeToCall);
-    };
+var onNextFrame = hasRAF ? function (callback) {
+    return window.requestAnimationFrame(callback);
+} : function (callback) {
+    var currentTime = Date.now();
+    var timeToCall = Math.max(0, 16.7 - (currentTime - prevTime));
+    prevTime = currentTime + timeToCall;
+    setTimeout(function () {
+        return callback(prevTime);
+    }, timeToCall);
+};
 
 function createRenderStep(startRenderLoop) {
     var functionsToRun = [];
@@ -34,7 +36,9 @@ function createRenderStep(startRenderLoop) {
             var _a;
         },
         schedule: function (callback, immediate) {
-            if (immediate === void 0) { immediate = false; }
+            if (immediate === void 0) {
+                immediate = false;
+            }
             startRenderLoop();
             var addToCurrentBuffer = immediate && isProcessing;
             var buffer = addToCurrentBuffer ? functionsToRun : functionsToRunNextFrame;
@@ -44,12 +48,16 @@ function createRenderStep(startRenderLoop) {
                     numThisFrame = functionsToRun.length;
                 }
             }
-        },
+        }
     };
 }
 
 var HAS_PERFORMANCE_NOW = typeof performance !== 'undefined' && performance.now !== undefined;
-var currentTime = HAS_PERFORMANCE_NOW ? function () { return performance.now(); } : function () { return Date.now(); };
+var currentTime = HAS_PERFORMANCE_NOW ? function () {
+    return performance.now();
+} : function () {
+    return Date.now();
+};
 var willRenderNextFrame = false;
 var MAX_ELAPSED = 40;
 var defaultElapsed = 16.7;
@@ -57,30 +65,25 @@ var useDefaultElapsed = true;
 var currentFramestamp = 0;
 var elapsed = 0;
 function startRenderLoop() {
-    if (willRenderNextFrame)
-        return;
+    if (willRenderNextFrame) return;
     willRenderNextFrame = true;
     useDefaultElapsed = true;
     onNextFrame(processFrame);
 }
-var frameStart = createRenderStep(startRenderLoop);
-var frameUpdate = createRenderStep(startRenderLoop);
-var frameRender = createRenderStep(startRenderLoop);
-var frameEnd = createRenderStep(startRenderLoop);
+var frameStart = /*#__PURE__*/createRenderStep(startRenderLoop);
+var frameUpdate = /*#__PURE__*/createRenderStep(startRenderLoop);
+var frameRender = /*#__PURE__*/createRenderStep(startRenderLoop);
+var frameEnd = /*#__PURE__*/createRenderStep(startRenderLoop);
 function processFrame(framestamp) {
     willRenderNextFrame = false;
-    elapsed = useDefaultElapsed
-        ? defaultElapsed
-        : Math.max(Math.min(framestamp - currentFramestamp, MAX_ELAPSED), 1);
-    if (!useDefaultElapsed)
-        defaultElapsed = elapsed;
+    elapsed = useDefaultElapsed ? defaultElapsed : Math.max(Math.min(framestamp - currentFramestamp, MAX_ELAPSED), 1);
+    if (!useDefaultElapsed) defaultElapsed = elapsed;
     currentFramestamp = framestamp;
     frameStart.process();
     frameUpdate.process();
     frameRender.process();
     frameEnd.process();
-    if (willRenderNextFrame)
-        useDefaultElapsed = false;
+    if (willRenderNextFrame) useDefaultElapsed = false;
 }
 var onFrameStart = frameStart.schedule;
 var onFrameUpdate = frameUpdate.schedule;
@@ -90,7 +93,11 @@ var cancelOnFrameStart = frameStart.cancel;
 var cancelOnFrameUpdate = frameUpdate.cancel;
 var cancelOnFrameRender = frameRender.cancel;
 var cancelOnFrameEnd = frameEnd.cancel;
-var timeSinceLastFrame = function () { return elapsed; };
-var currentFrameTime = function () { return currentFramestamp; };
+var timeSinceLastFrame = function () {
+    return elapsed;
+};
+var currentFrameTime = function () {
+    return currentFramestamp;
+};
 
 export { currentTime, onFrameStart, onFrameUpdate, onFrameRender, onFrameEnd, cancelOnFrameStart, cancelOnFrameUpdate, cancelOnFrameRender, cancelOnFrameEnd, timeSinceLastFrame, currentFrameTime };
